af801f9fdf1426e28e1ac34e5d6ebd25047420fe
Markus Teivo <markus.teivo@aalto.fi>
2020-05-22 21:30:19 +0300

    build folder mukaan

---
+++ build/precache-manifest.f4fa9be6cfb554c0f275fd2ea4c5fa2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71509f496ad74d93750e343d02a7586",
+    "url": "/index.html"
+  },
+  {
+    "revision": "835a21b9a55d0167e11b",
+    "url": "/static/css/main.abd2912e.chunk.css"
+  },
+  {
+    "revision": "37066464a2b84bbf03ea",
+    "url": "/static/js/2.ab1df740.chunk.js"
+  },
+  {
+    "revision": "835a21b9a55d0167e11b",
+    "url": "/static/js/main.f403a056.chunk.js"
+  },
+  {
+    "revision": "269ad4d029cce48fc3f0",
+    "url": "/static/js/runtime-main.b44955d6.js"
+  }
+]);
